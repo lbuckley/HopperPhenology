928f69f2380974b2fb8c30d0e62b4bce3ccd3181
L Buckley <lbbuckley@gmail.com>
2019-01-18 16:22:44 -0800

    Update comnmunity analysis to examine diapause

---
+++ GenCommunityData.R
@@ -0,0 +1,263 @@
+#Abbreviate to focus on summer GDDs and quantiles
+
+#load libraries
+library(ggplot2)
+library(plyr) 
+library(dplyr)
+library(colorRamps)
+require(cowplot)
+
+sites= c("CHA", "A1", "B1", "C1", "D1")  #Redfox: 1574
+elevs= c(1752, 2195, 2591, 3048, 3739)
+
+#source degree days function
+setwd("/Users/laurenbuckley/HopperPhenology/")
+source("degreedays.R")
+
+#======================================================
+#READ DATA
+
+fdir= "/Volumes/GoogleDrive/My\ Drive/AlexanderResurvey/DataForAnalysis/"
+
+#load climate data
+setwd( paste(fdir, "climate", sep="") )   
+clim= read.csv("AlexanderClimateAll_filled_May2018.csv")
+
+#---------------------
+#cummulative degree days
+#cumsum within groups
+clim = clim %>% group_by(Year,Site) %>% arrange(Julian) %>% mutate(cdd_sum = cumsum(dd_sum), cdd_sumfall = cumsum(dd_sumfall), cdd_july = cumsum(dd_july)) 
+# cdd_june = cumsum(dd_june),cdd_july = cumsum(dd_july),cdd_aug = cumsum(dd_aug),cdd_early = cumsum(dd_early),cdd_mid = cumsum(dd_mid),cdd_ac = cumsum(dd_ac),cdd_mb = cumsum(dd_mb),cdd_ms = cumsum(dd_ms)
+
+#load hopper data
+setwd( paste(fdir, "grasshoppers/SexCombined/", sep="") )
+hop= read.csv("HopperData_May2018.csv")
+
+#======================================================
+#CALCULATE GDD METRICS
+
+#subset years to survey
+clim1= clim[which(clim$Year %in% c(1958, 1959, 1960, 2006:2015))  ,]
+
+#set temp outside summer to NA
+clim1$Mean= (clim1$Min + clim1$Max)/2
+clim1$Mean_summer= clim1$Mean
+clim1[clim1$Julian<60 | clim1$Julian>243,"Mean_summer"]=NA
+
+cdat=clim1
+
+#metrics across years
+clim1= ddply(clim1, c("Site", "Year"), summarise,
+             Mean = mean(Mean_summer, na.rm=TRUE), Cdd_seas = max(cdd_sum, na.rm=FALSE), Cdd_seasfall = max(cdd_sumfall, na.rm=FALSE), Cdd_july = max(cdd_july, na.rm=TRUE) )
+         #    Mean = mean(Mean_summer, na.rm=TRUE), Sd = sd(Mean_summer, na.rm=TRUE),Cdd_seas = max(cdd_sum, na.rm=TRUE),Cdd_june = max(cdd_june, na.rm=TRUE),Cdd_july = max(cdd_july, na.rm=TRUE),Cdd_aug = max(cdd_aug, na.rm=TRUE),Cdd_early = max(cdd_early, na.rm=TRUE),Cdd_mid = max(cdd_mid, na.rm=TRUE),Cdd_ac = max(cdd_ac, na.rm=TRUE),Cdd_mb = max(cdd_mb, na.rm=TRUE),Cdd_ms = max(cdd_ms, na.rm=TRUE), Sem = sd(Mean_summer, na.rm=TRUE)/sqrt(length(na.omit(Mean_summer))))
+
+#PLOTS
+#temp
+ggplot(data=clim1, aes(x=Year, y = Mean, color=Site ))+geom_point()+geom_line() +theme_bw()
+#cum dd
+ggplot(data=clim1, aes(x=Year, y = Cdd_seas, color=Site ))+geom_point()+geom_line() +theme_bw()
+
+#---------
+# Jadult and GDDadult ~year by sites
+
+#subset to dates with adults
+hop1= hop[which(hop$in6>0),]
+
+#subset to focal species
+#specs= c("Aeropedellus clavatus","Chloealtis abdominalis","Camnula pellucida","Melanoplus dawsoni","Melanoplus boulderensis","Melanoplus sanguinipes")
+
+#hop1= hop1[which(hop1$species %in% specs ),]
+
+#trim columns
+hop1= hop1[,c("ordinal","species","in6","in5","in4","in3","in2","in1","total","site","period","year","sjy","cdd_sum")]
+
+#-------------
+## calculate median across individuals
+hop1= hop1[order(hop1$ordinal),]
+#cumulative sum of individuals within groups
+hop1 = hop1 %>% group_by(species,site,year) %>% arrange(species,site,year,ordinal) %>% mutate(csind = cumsum(in6))
+#number of median individual
+hop3 = hop1 %>% group_by(species,site,year) %>% arrange(species,site,year,ordinal) %>% mutate(medind = max(csind)/2, qlowind=max(csind)*0.15, qupind=max(csind)*0.85)
+
+#date of median individual
+hop3$inddif= abs(hop3$medind-hop3$csind) #difference from median individual
+hop3$inddif.qlow= abs(hop3$qlowind-hop3$csind) #difference from q20 individual
+hop3$inddif.qup= abs(hop3$qupind-hop3$csind) #difference from q80 individual
+
+hop4= do.call(rbind,lapply(split(hop3,list(hop3$species, hop3$site, hop3$year)),function(chunk) chunk[which.min(chunk$inddif),]))
+hop4.qlow= do.call(rbind,lapply(split(hop3,list(hop3$species, hop3$site, hop3$year)),function(chunk) chunk[which.min(chunk$inddif.qlow),]))
+hop4.qup= do.call(rbind,lapply(split(hop3,list(hop3$species, hop3$site, hop3$year)),function(chunk) chunk[which.min(chunk$inddif.qup),]))
+
+#plot low and high percentiles
+#combine
+hop4$quantile=50
+hop4.qup$quantile=85
+hop4.qlow$quantile=15
+hop5= rbind(hop4,hop4.qup, hop4.qlow)
+hop5$quantile= as.factor(hop5$quantile)
+hop5$yr_q= paste(hop5$year, hop5$quantile, sep="_")
+hop5$year= as.factor(hop5$year)
+
+hop4=hop5 #add quantiles 
+
+#-----------------
+# match phenology to temp and dd
+clim1$siteyear= paste(clim1$Site, clim1$Year, sep="")
+hop4$siteyear= paste(hop4$site, hop4$year, sep="")
+
+hop4$Tmean<-NA
+match1= match(hop4$siteyear, clim1$siteyear)
+matched= which(!is.na(match1))
+hop4$Tmean[matched]<- clim1$Mean[match1[matched]]  
+
+hop4$cdd_seas<-NA
+hop4$cdd_seas[matched]<- clim1$Cdd_seas[match1[matched]]  
+
+#---------------
+#add elevation
+hop4$elevation= as.factor(elevs[match(hop4$site, sites)])
+
+#---------------
+#set up clim var
+
+#GDDS
+#add elevation
+clim1$elevation= elevs[match(clim1$Site, sites)]
+#add period
+clim1$period="resurvey"
+clim1$period[which(clim1$Year<2000)]<-"initial"
+
+#order varaibles
+#period
+clim1$period= factor(clim1$period, levels=c("resurvey", "initial") )
+
+#Order by average across sites with complete gdd data
+clim.ave= subset(clim1, clim1$Site %in% c("B1","C1"))
+clim.ave= aggregate(clim.ave, list(clim.ave$Year),FUN=mean )
+clim1$Cdd_siteave= clim.ave$Cdd_seas[match(clim1$Year, clim.ave$Year)]
+clim1$Cdd_july_siteave= clim.ave$Cdd_july[match(clim1$Year, clim.ave$Year)]
+
+#------------------------------------------------
+#CALCULATE DEVELOMENT INDEX
+
+dat=hop[,c("ordinal","species","in6","in5","in4","in3","in2","in1","total","year","site","period","sjy","dd","dd_sum","cdd_sum","cdd_sumfall")]
+
+#replace instar NAs with zero
+dat[which(is.na(dat$in1)), "in1"]=0 
+dat[which(is.na(dat$in2)), "in2"]=0
+dat[which(is.na(dat$in3)), "in3"]=0
+dat[which(is.na(dat$in4)), "in4"]=0
+dat[which(is.na(dat$in5)), "in5"]=0
+dat[which(is.na(dat$in6)), "in6"]=0
+
+#Calculate development index
+dat$DI=0
+inds=which(dat$total>0)  
+dat$DI[inds]= (dat$in1[inds] +dat$in2[inds]*2 +dat$in3[inds]*3 +dat$in4[inds]*4 +dat$in5[inds]*5 +dat$in6[inds]*6)/dat$total[inds]
+
+#----------
+# specs= c("Aeropedellus clavatus", "Camnula pellucida", "Melanoplus dawsoni", "Melanoplus sanguinipes", "Melanoplus boulderensis")
+# #reduce to focal species
+#dat= dat[dat$species %in% specs,] #[3:6]
+#order
+#dat$species= ordered(dat$species, levels=c("Aeropedellus clavatus","Melanoplus boulderensis","Chloealtis abdominalis", "Camnula pellucida","Melanoplus sanguinipes","Melanoplus dawsoni") )
+
+#code period
+dat$per=1
+dat$per[dat$year>2000]=2
+
+# #code early and late species
+# dat$early_late=2
+# dat$early_late[dat$species %in% specs[c(2,4)]]=1
+
+#add elevation
+dat$elev= as.factor(elevs[match(dat$site, sites)])
+
+#add seasonal GDDs
+dat$siteyear= paste(dat$site, dat$year, sep="")
+
+dat$Tmean<-NA
+match1= match(dat$siteyear, clim1$siteyear)
+matched= which(!is.na(match1))
+dat$Tmean[matched]<- clim1$Mean[match1[matched]]  
+
+dat$cdd_seas<-NA
+dat$cdd_seas[matched]<- clim1$Cdd_seas[match1[matched]]  
+
+dat$Cdd_siteave<-NA
+dat$Cdd_siteave[matched]<- clim1$Cdd_siteave[match1[matched]]  
+
+dat$Cdd_july_siteave<-NA
+dat$Cdd_july_siteave[matched]<- clim1$Cdd_july_siteave[match1[matched]]  
+
+#clean up
+dat$year= as.factor(dat$year)
+
+#get rid of entries with low sample size: at least three individuals, #but keep if all first instar or adult
+drop= which(dat$total<3) # & dat$DI!=1 & dat$DI!=6
+if(length(drop)>0) dat=dat[-drop,]
+
+#order varaibles
+#period
+dat$period= factor(dat$period, levels=c("resurvey", "initial") )
+#elevation
+dat$elev= factor(dat$elev, levels=c(3048,2591,2195,1752) )
+#species
+#dat$species= factor(dat$species, levels=c("Aeropedellus clavatus","Melanoplus boulderensis","Chloealtis abdominalis", "Camnula pellucida", "Melanoplus sanguinipes", "Melanoplus dawsoni") )
+
+#------------------------------------------------
+#ESTIMATE ADULTHOOD BASED ON DI
+
+## failed using broom
+#library(broom)
+#broom::augment(x=fm1, newdata = Data, type.predict = "response")
+
+#Indexed calculation
+dat$spsiteyear= paste(dat$siteyear, dat$species, sep="")
+combs= unique(dat$spsiteyear)
+
+#days to predict over
+doys= 150:265
+
+#make matrix to store output
+dout= data.frame(spsiteyear=combs, doy_adult= rep(NA, length(combs)),gdd_adult= rep(NA, length(combs)) ) 
+
+for(k in 1:length(combs)){
+  dats= subset(dat, dat$spsiteyear==combs[k])
+  
+  #require at least 5 data points
+  if(nrow(dats)>=5) { 
+    #doy
+    doys= seq(min(dats$ordinal), max(dats$ordinal+7),5)
+    
+    spl<- smooth.spline(x=dats$ordinal, y=dats$DI)
+    pred.spl<- predict(spl, doys)
+    #extract point where almost all adults DI>5.5
+    dout[k,2]= doys[which.max(pred.spl$y>5.5)]
+    
+    #gdd
+    #restrict to observed gdds
+    gdds= seq(min(dats$cdd_sumfall), max(dats$cdd_sumfall+50),10)
+    
+    spl<- smooth.spline(x=dats$cdd_sumfall, y=dats$DI)
+    pred.spl<- predict(spl, gdds)
+    #extract point where almost all adults DI>5.5
+    dout[k,3]= gdds[which.max(pred.spl$y>5.5)]
+    
+  } #end check length
+  
+} #end combs
+
+#add estimate back to df
+dat$doy_adult= dout[match(dat$spsiteyear, dout$spsiteyear),"doy_adult"]
+dat$gdd_adult= dout[match(dat$spsiteyear, dout$spsiteyear),"gdd_adult"]
+
+#==============================================
+#WRITE SPECIES DATA
+
+setwd("/Volumes/GoogleDrive/My Drive/Buckley/Work/GrasshopperPhenSynch/data/")
+write.csv(dat, "HopperClimateData.csv")
+
+#==============================================
+
+
---
+++ HopperPhenologyOverlap_16Jan2019.R
@@ -0,0 +1,402 @@
+library(reshape)
+library(reshape2)
+library(tidyr)
+
+#LOAD SPECIES DATA
+setwd("/Volumes/GoogleDrive/My Drive/Buckley/Work/GrasshopperPhenSynch/data/")
+spec.diapause= read.csv("SpeciesDiapause.csv")
+dat.all= read.csv("HopperClimateData.csv")
+#data currently from phenology file PhenFigs_20May2018.R
+
+setwd("/Volumes/GoogleDrive/My Drive/Buckley/Work/GrasshopperPhenSynch/figures/")
+
+#------------------------------------
+#compare egg diapausers vs non
+dat.all$diapause="egg"
+dat.all$species= as.character(dat.all$species)
+dat.all$diapause[which(dat.all$species %in% c("Arphia conspersa","Eritettix simplex","Pardalaphoa apiculata","Xanthippus corallipes"))]="nymph"
+
+#focal species
+specs= c("Aeropedellus clavatus","Camnula pellucida","Melanoplus dawsoni","Melanoplus boulderensis","Melanoplus sanguinipes","Arphia conspersa","Eritettix simplex","Pardalaphoa apiculata","Xanthippus corallipes") #"Chloealtis abdominalis",
+#subset to focal species
+dat= subset(dat.all, dat.all$species %in% specs)
+
+years= unique(na.omit(hop$year))
+sites= unique(hop$site)
+
+#elevs and sites
+sites=c("CHA", "A1", "B1", "C1")
+elevs= c(1752, 2195, 2591, 3048)
+
+#Calculate development index
+dat$DIp=0
+inds=which(dat$total>0)  
+dat$DIp[inds]= (dat$in1[inds] +dat$in2[inds]*2 +dat$in3[inds]*3 +dat$in4[inds]*4 +dat$in5[inds]*5 +dat$in6[inds]*6)/6
+
+dat$spsiteyear= paste(dat$siteyear, dat$species, sep="")
+
+#calculate proportion
+dat.sum= ddply(dat, c("site", "year","species","spsiteyear"), summarise,
+               DIp = sum(DIp, na.rm=TRUE), TotMean=mean(total, na.rm=TRUE), TotSum=sum(total, na.rm=TRUE), AdultSum=sum(in6, na.rm=TRUE), Ndates=length(total[DIp>0]) )
+dat.sum= dat.sum[order(dat.sum$site,dat.sum$species, dat.sum$year),]
+
+dat$DIptotal=NA
+dat$Ndates= NA
+dat$TotSum= NA
+dat$AdultSum= NA
+match1= match(dat$spsiteyear, dat.sum$spsiteyear)
+matched= which(!is.na(match1))
+dat$DIptotal[matched]<- dat.sum$DIp[match1[matched]]  
+dat$Ndates[matched]<- dat.sum$Ndates[match1[matched]]
+dat$TotSum[matched]<- dat.sum$TotSum[match1[matched]]
+dat$AdultSum[matched]<- dat.sum$AdultSum[match1[matched]]
+#normalize
+dat$DIpNorm= dat$DIp/dat$DIptotal
+dat$AdultNorm= dat$in6/dat$AdultSum
+
+#drop cases with TotSum<30 or Ndates<5
+dat= dat[which(dat$Ndates>5),]
+dat= dat[which(dat$TotSum>30),]
+
+#all species combinations
+sp.combs= combn(specs, 2)
+#switch focal species
+sp.combs= cbind(sp.combs, sp.combs[c(2,1),])
+
+#set up data storage
+po= array(data = 0, dim = c(length(sp.combs[1,]), length(years), length(sites),10) ) #add dimension for multiple metrics
+
+#combine species combinations names
+sp.combs.n= paste(sp.combs[1,],sp.combs[2,],sep="_")
+
+#add po names
+dimnames(po)[[1]] <- sp.combs.n
+dimnames(po)[[2]] <- years
+dimnames(po)[[3]] <- sites
+
+#----
+# plot out densities
+#made season warmth a factor
+#make cdd_july index
+#dat$swarm= as.factor(round(dat$Cdd_siteave,2))
+dat$swarm= as.factor(round(dat$Cdd_july_siteave,2))
+
+#adults y=In6
+#DIp y= DIp
+
+#plot as proportion of total
+dat$elev.lab= factor(dat$elev.lab, levels=c("1752m","2195m","2591m","3048m"))
+dat$diapause= as.factor(dat$diapause)
+
+pdf("Fig_dipnorm.pdf",height = 11, width = 10)
+ggplot(data=dat, aes(x=ordinal, y = DIpNorm, group=species, color=species, lty=diapause))+
+  geom_smooth(method="loess", se=FALSE)+geom_point()+facet_grid(year~elev.lab, scales="free")+ylim(0,0.2)+
+  ylab("Proportional abundance")+xlab("Day of year")+ theme(legend.position="bottom")+
+  guides(color=guide_legend(nrow=2,byrow=TRUE))
+dev.off()
+
+pdf("Fig_dipnormyear.pdf",height = 10, width = 10)
+ggplot(data=dat, aes(x=ordinal, y = DIpNorm, group=species, color=species))+
+  geom_smooth(method="loess", se=FALSE)+geom_point()+facet_grid(year~elev.lab, scales="free")+ylim(0,0.2)+ylab("Proportional abundance")+xlab("Day of year")
+dev.off()
+
+#---
+#by species
+pdf("Fig_distnormyear_byspec.pdf",height = 12, width = 10)
+ggplot(data=dat, aes(x=ordinal, y = DIpNorm, group=spsiteyear, color=Cdd_siteave))+
+  geom_smooth(method="loess", se=FALSE)+geom_point()+facet_grid(species~site, scales="free")+ylim(0,0.2)
+dev.off()
+
+#----------------------------
+for(year in 1:length(years)){
+  for(site in 1:length(sites)){
+    
+    #sum all individuals by species and year 
+    dat.sub= dat[dat$year==years[year],]
+    dat.sub= dat.sub[ which(dat.sub$site==sites[site]),]
+    #restrict to adults
+    #dat.sub= subset(dat.sub, dat.sub$in6>0 )
+    
+    #change to GDD
+    #dat.sub$ordinal= dat.sub$cdd_sumfall
+    
+    if( nrow(dat.sub)>0 ){ #check data exists
+      
+      ords= unique(dat.sub$ordinal)
+      
+      #totals by ordinal date
+      dat.agg= dat.sub[,c("species", "in6")] #extract adults
+      dat.agg$Species= as.factor(dat.sub$species)
+      dat.agg= aggregate(dat.agg$in6, by=list(dat.agg$species), FUN=sum) #sum number adults
+      names(dat.agg)= c("Species", "YrTot")
+      
+      #turn into proportions
+      match1= match( dat.sub$species, dat.agg$Species)
+      dat.sub$YrTot= dat.agg$YrTot[match1] #Add total number adults per species per year
+      dat.sub= dat.sub[which(dat.sub$YrTot>0),]
+      dat.sub$AdultProp= dat.sub$in6/dat.sub$YrTot #Add proportion of annual individuals for each ordinal date, normalize species with different abundances 
+      #subset to dates with adults
+      dat.sub= dat.sub[which(dat.sub$AdultProp>0),]
+      
+      #find species combinations potentially present
+      specs1= unique(dat.sub$species)
+      #find species combinations present
+      inds=which(sp.combs[1,]%in%specs1 & sp.combs[2,]%in%specs1)
+      
+      #loop through species combinations
+      for(ind.k in 1:length(inds) ){
+        sp1= sp.combs[1,inds[ind.k] ]
+        sp2= sp.combs[2,inds[ind.k] ]
+        #subset to combinations
+        dat.sub2= dat.sub[which(dat.sub$species==sp1 | dat.sub$species==sp2),]
+        
+        #   #Pianka (1974) metric, http://www.jstor.org/stable/pdf/4217327.pdf, 
+        
+        #new metric?: sum across dates: (max proportion-overlap)/total, On the Analysis of Phenological Overlap
+        #   #po[inds[c], year, site]= po[inds[c], year, site]+ (max(prop1,prop2)-abs(prop1-prop2))/(prop1+prop2)
+       
+        #DI overlap
+       
+        #check each species has at least 4 dates
+        if( length(which(dat.sub2$species==sp1))>3 & length(which(dat.sub2$species==sp2))>3 ){
+        
+        #loess curve for each species
+        d.sp1= dat.sub2[which(dat.sub2$species==sp1),]
+        d.sp2= dat.sub2[which(dat.sub2$species==sp2),]
+        
+        # create loess object and prediction function
+        l1 <- loess(DIp~ordinal, d.sp1)
+        l2 <- loess(DIp~ordinal, d.sp2)
+        
+        #set negative values and NA to zero
+        f1 <- function(x) {f=predict(l1,newdata=x); f[f<0]=0;f[is.na(f)]=0; return(f)}
+        f2 <- function(x) {f=predict(l2,newdata=x); f[f<0]=0;f[is.na(f)]=0; return(f)}
+        
+        #find range
+        low= min(dat.sub2$ordinal)
+        up= max(dat.sub2$ordinal)
+       
+        # perform integration
+        a1=integrate(f1,low,up)$value
+        a2=integrate(f2,low,up)$value
+     
+        #area overlap
+        #normalize to 1
+        sp1.n= f1(low:up)/a1
+        sp2.n= f2(low:up)/a2
+        #pianka's index
+        po[inds[ind.k], year, site,1]= sum(sp1.n*sp2.n)/sqrt(sum(sp1.n^2)*sum(sp2.n^2))
+        
+        #overlapping area normalized to total area
+        #normalize to 1
+        sp1.n= f1(low:up)
+        sp2.n= f2(low:up)
+        dinds= which(sp1.n>0 & sp2.n>0)
+        if(length(dinds>0)) po[inds[ind.k], year, site,2]= sum(sapply(dinds, FUN=function(x) min(sp1.n[x],sp2.n[x])) )/(a1+a2)
+        #overlapping area normalized to total area of focal species
+        if(length(dinds>0)) po[inds[ind.k], year, site,3]= sum(sapply(dinds, FUN=function(x) min(sp1.n[x],sp2.n[x])) )/(a1)
+        #not normalized
+        if(length(dinds>0)) po[inds[ind.k], year, site,4]= sum(sapply(dinds, FUN=function(x) min(sp1.n[x],sp2.n[x])) )
+        
+        #area of focal species
+        po[inds[ind.k], year, site,5]= a1
+        
+        #time difference metrics
+        #days focal
+        po[inds[ind.k], year, site,6]= length(which(sp1.n>0))
+        #days overlap
+        po[inds[ind.k], year, site,7]= length(dinds)
+        #days overlap normalized
+        po[inds[ind.k], year, site,8]= length(dinds)/length(which(sp1.n>0))
+        #days difference in peak
+        po[inds[ind.k], year, site,9]= abs(which.max(sp1.n)-which.max(sp2.n))
+        #days difference in 20% of peak
+        po[inds[ind.k], year, site,10]= abs(which.max(sp1.n>max(sp1.n)*0.2) - which.max(sp2.n>max(sp2.n)*0.2) )
+        
+        } #end check each species has at least 4 dates
+      } #end loop species combinations
+      
+    } #end check data
+  } #end loop year
+} #end loop sites
+
+#replace 0 values with NA
+po[po==0]=NA
+
+#-----------------------------------
+#PLOTS
+# PO: sp comb x years x sites
+
+po1= melt(po, varnames=c("sp","year","site","metric"))
+
+#drop NAs
+po1= po1[which(!is.na(po1$value)),]
+
+#add time period
+po1$period="initial"
+po1[which(po1$year>1960),"period"]<-"resurvey"
+
+#ADD cdd and temp
+clim1$siteyear= paste(clim1$Site, clim1$Year, sep="")
+po1$siteyear= paste(po1$site, po1$year, sep="")
+
+#select cdd metric
+#clim1$Cdd=  clim1$Cdd_sum
+#clim1$Cdd=  clim1$Cdd_june
+#clim1$Cdd=  clim1$Cdd_july
+#clim1$Cdd=  clim1$Cdd_aug
+#clim1$Cdd=  clim1$Cdd_early
+#clim1$Cdd=  clim1$Cdd_mid
+clim1$Cdd= clim1$Cdd_seas
+
+po1$Tmean=NA
+po1$cdd=NA
+po1$cdd_july=NA
+match1= match(po1$siteyear, clim1$siteyear)
+matched= which(!is.na(match1))
+po1$Tmean[matched]<- clim1$Mean[match1[matched]]  
+po1$cdd[matched]<- clim1$Cdd[match1[matched]]
+po1$cdd_july[matched]<- clim1$Cdd_july[match1[matched]]
+
+# ##match back to dat
+# datm= dat[duplicated(dat$siteyear)==FALSE,]
+# match1= match(po1$siteyear, datm$siteyear)
+# matched= which(!is.na(match1))
+# po1$Tmean[matched]<- datm$Tmean[match1[matched]]  
+# po1$cdd[matched]<- datm$cdd_seas[match1[matched]] 
+
+#---
+#Change years for plotting ### FIX
+po1[which(po1$year==1958),"year"]= 1958+40
+po1[which(po1$year==1959),"year"]= 1959+40
+po1[which(po1$year==1960),"year"]= 1960+40
+
+#separate species names for plotting
+po1$sp= as.character(po1$sp)
+
+sps= colsplit(po1$sp, pattern = "_", names = c('sp1', 'sp2'))
+po1$sp1<- sps$sp1
+po1$sp2<- sps$sp2
+
+#order by early and late species
+#ave phen
+#hop.el = hop1 %>% group_by(species,site) %>% arrange(species,site) %>% mutate(phen = mean(ordinal))
+hop.agg= aggregate(hop1, list(hop1$species),FUN=mean) #fix for hop1$site,
+hop.agg= hop.agg[order(hop.agg$ordinal),]
+
+#make species factor for plotting
+#po1$sp1= factor(po1$sp1, levels=hop.agg$Group.1)
+#po1$sp2= factor(po1$sp2, levels=hop.agg$Group.1)
+po1$sp1= factor(po1$sp1, levels=c("Aeropedellus clavatus","Melanoplus boulderensis","Camnula pellucida","Melanoplus sanguinipes", "Melanoplus dawsoni", "Chloealtis abdominalis"))
+po1$sp2= factor(po1$sp2, levels=c("Aeropedellus clavatus","Melanoplus boulderensis","Camnula pellucida","Melanoplus sanguinipes", "Melanoplus dawsoni", "Chloealtis abdominalis"))
+
+#PLOT
+#add elevation
+po1$elevation= as.factor(elevs[match(po1$site, sites)])
+
+#drop Chataqua for now 
+#po1= po1[which(po1$site!="CHA"),]
+
+#focus on sites B1 and C1 for now
+#po1= po1[which(po1$site %in% c("B1", "C1")) ,] #[which(po1$metric==2),] #
+
+#*********************************************
+#Plot overlap
+
+#assess significance
+#calculate significant regressions
+#apply through combinations of species and elevations
+po1$elevspec= paste(po1$elevation, po1$sp, sep="")
+elevspec= matrix(unique(po1$elevspec))
+
+#---
+#EXTRACT COEFFS
+po2= po1[which(po1$metric==7),] #3,7
+x1="cdd_july"
+#x1="year"
+#x1="Tmean"
+
+p.gdd= apply(elevspec,1, FUN=function(x) summary(lm(value~get(x1), data=po2[which(po2$elevation==substr(x,1,4)& po2$sp==substr(x,5,nchar(x)) ),] ))$coefficients[2,])
+
+#combine
+p.mat=as.data.frame(cbind(elevspec,t(p.gdd) ))
+#make numeric
+p.mat$Estimate= as.numeric(as.character(p.mat$Estimate))
+p.mat$`Std. Error`= as.numeric(as.character(p.mat$`Std. Error`))
+#CIs
+p.mat$upCI= p.mat$Estimate + 1.96*p.mat$`Std. Error`
+p.mat$lowCI= p.mat$Estimate - 1.96*p.mat$`Std. Error`
+
+#add columns for significance
+p.mat$sig.gdd<-"nonsignificant"
+p.mat$sig.gdd[which(p.gdd[4,]<0.05)]="significant"
+
+#add back to matrix
+match1= match(po2$elevspec, elevspec)
+po2$sig.gdd= factor(p.mat[match1,"sig.gdd"], levels=c("significant","nonsignificant"))
+#---
+
+pdf("PhenOverlap_byGDD_days.pdf", height = 8, width = 10)
+ggplot(data=po2, aes_string(x=x1, y = "value", color="elevation"))+geom_point(aes(shape=period, fill=period), size=2)+
+  facet_grid(sp1~sp2, drop=TRUE)+theme_bw()+geom_smooth(method="lm", se=FALSE, aes(linetype=sig.gdd))+ 
+  scale_color_manual(values=c("darkorange", "blue","darkgreen","purple"))#+xlim(200,750)
+dev.off()
+
+#+ylab("Phenological overlap (days)")+xlab("Growing degree days")
+
+#**********************************************
+
+# #overlap by GDD by site
+# pdf("PhenOverlap_byGDD_site.pdf", height = 8, width = 10)
+# ggplot(data=po1[which(po1$metric==1),], aes(x=cdd, y = value, color=sp))+geom_point(aes(shape=period, fill=period), size=2)+
+#   facet_wrap(~site, drop=TRUE, scales="free")+theme_bw()+geom_smooth(method="lm", se=FALSE)+ylab("Phenological overlap")+xlab("Growing degree days")+ 
+#   scale_color_manual(values=rainbow(20))+ylim(0,1)
+# dev.off()
+
+#--------------
+#Average overlap across community
+
+po.comm= ddply(po1, c("site", "year","metric","period","siteyear"), summarise,
+               value = mean(value, na.rm=TRUE), Tmean= Tmean[1], cdd=cdd[1],cdd_july=cdd_july[1],elevation=elevation[1])
+                 
+ggplot(data=po.comm[which(po.comm$metric==1),], aes(x=year, y = value, color=elevation))+geom_point(aes(shape=period, fill=period), size=2)+
+  theme_bw()+geom_smooth(method="lm", se=FALSE)
+
+#===================================
+# STATS GROUP BY SPECIES
+po2= po1[which(po1$metric==4),] 
+
+mod1= lm(value~ cdd*elevation+sp2+sp1, data=po2)
+mod1= lm(value~ cdd*as.numeric(as.character(elevation))+sp, data=po2)
+
+#---------------
+#by focal species
+sp.k=5
+po3= subset(po2, po2$sp1==specs[sp.k] ) #subset(po2, po2$sp1==specs[sp.k] | po2$sp2==specs[sp.k])
+# #switch species order
+# p.temp= po3$sp1
+# inds= which(po3$sp2==specs[sp.k])
+# if(length(inds)>0){
+#  po3$sp1[inds]=specs[sp.k] 
+#  po3$sp2[inds]=po3$sp1[inds] 
+# }
+
+po3$elevation= as.numeric(as.character(po3$elevation))
+mod1= lm(value~ cdd_july+cdd_july:elevation+cdd_july:sp2, data=po3)
+specs[sp.k]
+summary(mod1)
+
+#---------------
+#early vs late
+require(nlme)
+require(lme4)
+
+po3$timing="early"
+po3$timing[which(po3$sp2 %in% specs[c(1,4)])]<-"late"
+
+#mod1= lme(value~ cdd_july+cdd_july:elevation,random=~1|sp2 , data=po3)
+mod1= lme(value~ cdd_july+cdd_july:elevation+cdd_july:timing,random=~1|sp2 , data=po3)
+summary(mod1)
+#--------------------------------
+
+
